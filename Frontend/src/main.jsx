ed2342db01180cfeb751bd87ac96b77c2bd85a5e
neeraj-bmj <neerajguptaica@gmail.com>
2025-09-22 15:05:25 +0530

    PWA = Prograssive Web Application added successful ✅

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import {registerSW} from "virtual:pwa-register";
+
+registerSW()
 
 createRoot(document.getElementById("root")).render(
   <>
